33c22376d045b955872dd2eafdbe76b1b2d55a9a
ledakaradimou <ledakaradimou@me.com>
2016-12-01 17:30:09 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,197 @@
+"""
+CS 411 Recipe Web App Project
+(part of the code was adapted from cs 460 project)
+"""
+
+import flask
+from flask import Flask, Response, request, render_template, redirect, url_for
+from flaskext.mysql import MySQL
+import flask_login
+
+#for image uploading
+from werkzeug import secure_filename
+import os, base64
+
+mysql = MySQL()
+app = Flask(__name__)
+app.secret_key = 'PRIdenkserw15991' #change this
+
+#These will need to be changed according to your creditionals
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'PRIdenkserw15991' #change this
+app.config['MYSQL_DATABASE_DB'] = 'recipedb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+#begin code used for login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+cursor.execute("SELECT email from Users") 
+users = cursor.fetchall()
+
+def getUserList():
+	cursor = conn.cursor()
+	cursor.execute("SELECT email from Users") 
+	return cursor.fetchall()
+
+class User(flask_login.UserMixin):
+	pass
+
+@login_manager.user_loader
+def user_loader(email):
+	users = getUserList()
+	if not(email) or email not in str(users):
+		return
+	user = User()
+	user.id = email
+	return user
+
+@login_manager.request_loader
+def request_loader(request):
+	users = getUserList()
+	email = request.form.get('email')
+	if not(email) or email not in str(users):
+		return
+	user = User()
+	user.id = email
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email))
+	data = cursor.fetchall()
+	pwd = str(data[0][0] )
+	user.is_authenticated = request.form['password'] == pwd 
+	return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if flask.request.method == 'GET':
+		return '''
+			   <form action='login' method='POST'>
+				<input type='text' name='email' id='email' placeholder='email'></input>
+				<input type='password' name='password' id='password' placeholder='password'></input>
+				<input type='submit' name='submit'></input>
+			   </form></br>
+		   <a href='/'>Home</a>
+			   '''
+	#The request method is POST (page is recieving data)
+	email = flask.request.form['email']
+	cursor = conn.cursor()
+	#check if email is registered
+	if cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email)):
+		data = cursor.fetchall()
+		pwd = str(data[0][0] )
+		if flask.request.form['password'] == pwd:
+			user = User()
+			user.id = email
+			flask_login.login_user(user) #okay login in user
+			return flask.redirect(flask.url_for('protected')) #protected is a function defined in this file
+
+	#information did not match
+	return "<a href='/login'>Try again</a>\
+			</br><a href='/register'>or make an account</a>"
+
+@app.route('/logout')
+def logout():
+	flask_login.logout_user()
+	return render_template('hello.html', message='Logged out') 
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+	return render_template('unauth.html') 
+
+#you can specify specific methods (GET/POST) in function header instead of inside the functions as seen earlier
+@app.route("/register", methods=['GET'])
+def register():
+	return render_template('register.html', supress=True)  
+
+def getUserIdFromEmail(email):
+	cursor = conn.cursor()
+	cursor.execute("SELECT user_id  FROM Users WHERE email = '{0}'".format(email))
+	return cursor.fetchone()[0]
+
+def getUserFirstNameFromID(user_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT firstname FROM Users WHERE user_id = '{0}'".format(user_id))
+	return cursor.fetchone()[0]
+
+def getUserLastNameFromID(user_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT lastname FROM Users WHERE user_id = '{0}'".format(user_id))
+	return cursor.fetchone()[0]
+
+
+def isEmailUnique(email):
+	#use this to check if a email has already been registered
+	cursor = conn.cursor()
+	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)): 
+		#this means there are greater than zero entries with that email
+		return False
+	else:
+		return True
+#end login code
+
+@app.route("/register", methods=['POST'])
+def register_user():
+	try:
+		firstname = request.form.get('firstname')
+		lastname = request.form.get('lastname')
+		dateofbirth = request.form.get('dateofbirth')
+		email=request.form.get('email')
+		password=request.form.get('password')
+	except:
+		print ("couldn't find all tokens") #this prints to shell, end users will not see this (all print statements go to shell)
+		return flask.redirect(flask.url_for('register'))
+	cursor = conn.cursor()
+	test =  isEmailUnique(email)
+	if test:
+		print(cursor.execute("INSERT INTO Users (firstname, lastname, dateofbirth, email, password) VALUES ('{0}', '{1}','{2}','{3}','{4}')".format(firstname, lastname, dateofbirth, email, password)))
+		conn.commit()
+		#log user in
+		user = User()
+		user.id = email
+		flask_login.login_user(user)
+		return render_template('hello.html', name=firstname, message='Account Created!')
+	else:
+		print ("couldn't find all tokens")
+		return flask.redirect(flask.url_for('register',message='ERROR!:User already exist'))
+
+#following somebody
+@app.route("/follow", methods= ['GET', 'POST'])
+@flask_login.login_required
+def follow_friend():
+	if request.method == 'POST':
+		user_id1 = getUserIdFromEmail(flask_login.current_user.id)
+		email = request.form.get('email')
+		test =  isEmailUnique(email)
+		if not test:
+			user_id2 = getUserIdFromEmail(email)
+			print(email)
+			cursor = conn.cursor()
+			try:
+				cursor.execute("INSERT INTO Follows (user_id1, user_id2) VALUES ('{0}', '{1}')".format(user_id1,user_id2))
+				conn.commit()
+				cursor.execute("INSERT INTO Follows (user_id1, user_id2) VALUES ('{0}', '{1}')".format(user_id2,user_id1))
+				conn.commit()
+				return render_template('hello.html', name=flask_login.current_user.id, message='Friend Followed!')
+			except:
+				print ("already following this person")
+				return render_template('follow.html', message='already following this person!!!!!!')
+		else: 
+			return render_template('hello.html',name=flask_login.current_user.id, message='Sorry, user does not exist')
+	else:
+		return render_template('follow.html')
+
+@app.route("/", methods=['GET'])
+def hello():
+	return render_template('hello.html', message='Welcome to our Recipe Web App!')
+
+
+
+if __name__ == "__main__":
+	#this is invoked when in the shell  you run 
+	#$ python app.py 
+	app.run(port=5000, debug=True)
+
